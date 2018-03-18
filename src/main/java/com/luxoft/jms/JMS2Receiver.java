7b19757caaf33a00ee2526f64d465e5804c47b91
Kireyko Vladimir <kireyko@gmail.com>
2018-03-18 23:00:45 +0200

    1

---
+++ src/main/java/com/luxoft/jms/JMS2Receiver.java
@@ -0,0 +1,85 @@
+package com.luxoft.jms;
+
+import com.sun.messaging.ConnectionFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.jms.*;
+
+public class JMS2Receiver {
+    private Logger logger = LogManager.getLogger(JMS2Receiver.class);
+
+    public static String[] getFilters() {
+        return filters;
+    }
+
+    private static String[] filters = {
+            "iban like 'DE%' and (amount <= 25 or amount >=75) ",
+            "iban like 'DE%' and (amount > 25 and amount <75 ) ",
+            "iban like 'FR%' and (amount <= 25 or amount >=75) ",
+            "iban like 'FR%' and (amount > 25 and amount <75 ) "
+    };
+
+    private static volatile int receiverNumber;
+    private int SumOnAccount;
+    private int sendStatus;
+
+    public  JMS2Receiver(String filter) {
+        //this.SumOnAccount=0;
+        threadReceiver(filter);
+    }
+
+    private  void threadReceiver(String filter){
+        int recN = receiverNumber;
+        receiverNumber++;
+        logger.info("[{}] receiver Number ", recN);
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        JMSContext jmsContext = connectionFactory.createContext();
+
+        Queue queue = jmsContext.createQueue("ACCOUNTS_2.QUEUE");
+        JMSConsumer jmsConsumer = jmsContext.createConsumer(queue, filter);
+
+        jmsConsumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                try {
+                    logger.info("[{}] Incoming message: {}",recN, ((TextMessage) message).getText());
+
+                    if (SumOnAccount<=100){
+                        SumOnAccount = SumOnAccount + ((TextMessage) message).getIntProperty("amount");
+                        logger.info("[{}] SumOnAccount:{}",recN, SumOnAccount);
+                    }
+
+                    if (SumOnAccount>100 & sendStatus==0) {
+                        logger.info("[{}] Sending response ... ", recN);
+                        sendStatus=1;
+
+                        String confirmation = message.getBody(String.class) ;
+                       // logger.info("Command execution confirmation: {}", confirmation);
+
+                        jmsContext.createProducer()
+                                //.setJMSCorrelationID(message.getJMSMessageID())
+                                .setProperty("receiverNumber", recN)
+                                .setProperty("SumOnAccount", SumOnAccount)
+                                .send(message.getJMSReplyTo(), confirmation);
+                    }
+
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        logger.info("[{}] Waiting on messages fulfilling the condition: {}",recN, jmsConsumer.getMessageSelector());
+    }
+
+    public static void main(String[] args) {
+        for (String filterElement: filters) {
+            new Thread() {
+                public void run() {
+                    new JMS2Receiver(filterElement);
+                }
+            }.start();
+        }
+    }
+}
---
+++ src/main/java/com/luxoft/jms/JMS2Sender.java
@@ -0,0 +1,58 @@
+package com.luxoft.jms;
+
+import com.sun.messaging.ConnectionConfiguration;
+import com.sun.messaging.ConnectionFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.jms.*;
+
+public class JMS2Sender {
+    private static volatile int numberOfConfirmations = 0;
+    private static int receiverNumber;
+
+    public static void main(String[] args) throws JMSException, InterruptedException {
+        Logger logger = LogManager.getLogger(JMS2Sender.class);
+
+        String[] ibans = new String[]{"DE443794221391961944346", "FR143841010373619987209"};
+        int[] receiverSumOnAccount = new int[5];
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setProperty(ConnectionConfiguration.imqAddressList, "mq://localhost:7676");
+        try (JMSContext jmsContext = connectionFactory.createContext();) {
+            Queue queueRequest = jmsContext.createQueue("ACCOUNTS_2.QUEUE");
+            Queue queueResponse = jmsContext.createQueue("ACCOUNTS_2.QUEUE.RESP");
+
+            JMSConsumer receiver = jmsContext.createConsumer(queueResponse);
+            receiver.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message confirmationMessage) {
+                    try {
+
+                        receiverNumber = confirmationMessage.getIntProperty("receiverNumber");
+                        receiverSumOnAccount[receiverNumber] = confirmationMessage.getIntProperty("SumOnAccount");
+                        logger.info("Confirmation({}) received from receiver {} with total sum : {}", numberOfConfirmations, receiverNumber, receiverSumOnAccount[receiverNumber]);
+                        numberOfConfirmations++;
+                    } catch (JMSException jmsException) {
+                        jmsException.printStackTrace();
+                    }
+                }
+            });
+
+            while (numberOfConfirmations < JMS2Receiver.getFilters().length) {
+                int amount = (int) (100 * Math.random());
+                TextMessage message = jmsContext.createTextMessage("DEPOSIT " + amount + " EUR into account " + ibans[amount % 2]);
+
+                jmsContext.createProducer()
+                        .setProperty("iban", ibans[amount % 2])
+                        .setProperty("amount", amount)
+                        .setJMSReplyTo(queueResponse)
+                        .send(queueRequest, message);
+                logger.info("Message send: {}", message.getText());
+
+            }
+        }
+    }
+
+
+}
